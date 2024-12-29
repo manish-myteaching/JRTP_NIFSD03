94493a3f86a05615b5c82a1f16888ad154462eca
manish-myteaching <manishverma.myteaching@gmail.com>
2024-12-29 16:35:56 +0530

    Test

---
+++ ISH_Major_Project/ISH-ApplicationRegistration_03_API/src/main/java/com/professionalit/IshApplicationRegistration03ApiApplication.java
@@ -0,0 +1,12 @@
+package com.professionalit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshApplicationRegistration03ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshApplicationRegistration03ApiApplication.class, args);
+	}
+}
---
+++ ISH_Major_Project/ISH-ApplicationRegistration_03_API/src/main/java/com/professionalit/exception/InvalidSSNException.java
@@ -0,0 +1,7 @@
+package com.professionalit.exception;
+
+public class InvalidSSNException extends RuntimeException {
+	public InvalidSSNException(String message) {
+		super(message);
+	}
+}
